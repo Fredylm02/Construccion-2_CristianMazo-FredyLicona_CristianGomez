b7463326807f5e24afe6c0327eae1d467ee4396c
Fredylm <irvingnets15@gmail.com>
2024-03-17 04:15:24 -0500

    Actualizacion

--- Veterinary/Veterinary/src/main/java/app/validators/MedicalRecordValidator.java
+++
@@ -1,9 +0,0 @@
-package app.validators;
-
-import app.dto.MedicalRecordDTO;
-
-public class MedicalRecordValidator {
-    public static boolean validateMedicalRecord(MedicalRecordDTO medicalRecordDTO) {
-        return true; // Supongamos que la validación siempre es exitosa por ahora
-    }
-}
--- Veterinary/Veterinary/src/main/java/app/validators/PetValidator.java
+++
@@ -1,8 +0,0 @@
-package app.validators;
-import app.dto.PetDTO;
-
-public class PetValidator {
-    public static boolean validatePet(PetDTO petDTO) {
-        return true; // Supongamos que la validación siempre es exitosa por ahora
-    }
-}
